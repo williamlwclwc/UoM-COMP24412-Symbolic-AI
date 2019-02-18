8d4d677ee39e849a4a3144fbe3150ac54263188d
Wenchang Liu <wenchang.liu@student.manchester.ac.uk>
2019-02-18 16:29:24 +0000

    passed online judge

--- ex1/family_relations.pl
+++ ex1/family_relations.pl
@@ -1,43 +1,43 @@
 % X is a parent of Y
 parent(X, Y) :-
-    parents(X, _ , Y);
-    parents(_, X, Y).
+    (parents(X, _, Y);
+    parents(_, X, Y)).
 
 % X is the father (a parent who is a man) of Y
-father(X, Y) :-
+father(X, Y) :-
     parent(X, Y),
     man(X).
 
 %  X is the mother (a parent who is a woman) of Y
-mother(X, Y) :- 
+mother(X, Y) :- 
     parent(X, Y),
     woman(X).
 
 % X and Y have at least one parent the same but are different people.
-sibling(X, Y) :-
+sibling(X, Y) :-
     parent(Z, X),
     parent(Z, Y),
     dif(X, Y).
 
 % X is a man and a sibling of Y
-brother(X, Y) :-
+brother(X, Y) :-
     man(X),
     sibling(X, Y).
 
 %  X is a woman and a sibling of Y
-sister(X, Y) :-
+sister(X, Y) :-
     woman(X),
     sibling(X, Y).
 
 % A parent of X is a sibling of the parent of Y and X and Y are different people.
-first_cousin(X, Y) :-
+first_cousin(X, Y) :-
     parent(A, X),
     parent(B, Y),
     sibling(A, B),
     dif(X, Y).
 
 % X is an ancestor of Y if X is Y’s parent or an ancestor of one of Y’s parents. A person should not be their own ancestor.
-ancestor(X, Y) :-
+ancestor(X, Y) :-
     parent(X, Y);
     (parent(Z, Y), ancestor(X, Z)).
 
--- ex1/wolf_puzzle.pl
+++ ex1/wolf_puzzle.pl
@@ -24,18 +24,18 @@ side_same(X, Y) :-
     X = Y.
 
 % track positon
-wfgc(_, _, _, _).
+fwgc(_, _, _, _).
 
 % check state
-is_state(wfgc(A, B, C, D)) :- 
+is_state(fwgc(A, B, C, D)) :- 
     is_pos(A),
     is_pos(B),
     is_pos(C),
     is_pos(D).
 
 % safe state: wolf not with goat & goat not with cabbage
-safe_state(wfgc(Farmer, Wolf, Goat, Cabbage)) :-
-    is_state(wfgc(Farmer, Wolf, Goat, Cabbage)),
+safestate(fwgc(Farmer, Wolf, Goat, Cabbage)) :-
+    is_state(fwgc(Farmer, Wolf, Goat, Cabbage)),
     (side_opposite(Wolf, Goat); side_same(Wolf, Farmer)),
     (side_opposite(Goat, Cabbage); side_same(Goat, Farmer)).
 
@@ -46,7 +46,7 @@ m(cabbage).
 m(wolf).
 
 % move: from old state to new state
-move(wfgc(Farmer, Wolf, Goat, Cabbage), M, wfgc(NewF, NewW, NewG, NewC)) :-
+move(fwgc(Farmer, Wolf, Goat, Cabbage), M, fwgc(NewF, NewW, NewG, NewC)) :-
     m(M),
     ((M = alone, dif(Farmer, NewF), Wolf = NewW, Goat = NewG, Cabbage = NewC);
     (M = goat, dif(Farmer, NewF), dif(Goat, NewG), Wolf = NewW, Cabbage = NewC);
@@ -54,22 +54,22 @@ move(wfgc(Farmer, Wolf, Goat, Cabbage), M, wfgc(NewF, NewW, NewG, NewC)) :-
     (M = wolf, dif(Farmer, NewF), dif(Wolf, NewW), Goat = NewG, Cabbage = NewC)).
 
 % get move results
-puzzlestate_move(wfgc(south, south, south, south), []).
-puzzlestate_move(wfgc(Farmer, Wolf, Goat, Cabbage), [M|Ms]) :-
+puzzlestate_moves(fwgc(south, south, south, south), []).
+puzzlestate_moves(fwgc(Farmer, Wolf, Goat, Cabbage), [M|Ms]) :-
     % Move: old state -> new state
-    safe_state(wfgc(Farmer, Wolf, Goat, Cabbage)),
-    safe_state(wfgc(OldF, OldW, OldG, OldC)),
-    move(wfgc(OldF, OldW, OldG, OldC), M, wfgc(Farmer, Wolf, Goat, Cabbage)),
-    puzzlestate_move(wfgc(OldF, OldW, OldG, OldC), Ms).
+    safestate(fwgc(Farmer, Wolf, Goat, Cabbage)),
+    safestate(fwgc(OldF, OldW, OldG, OldC)),
+    move(fwgc(OldF, OldW, OldG, OldC), M, fwgc(Farmer, Wolf, Goat, Cabbage)),
+    puzzlestate_moves(fwgc(OldF, OldW, OldG, OldC), Ms).
 
 % modify puzzlestate_move
-puzzlestate_move_without(wfgc(south, south, south, south), [], _Visited).
-puzzlestate_move_without(wfgc(Farmer, Wolf, Goat, Cabbage), [M|Ms], Visited) :-
+puzzlestate_moves_without(fwgc(south, south, south, south), [], _Visited).
+puzzlestate_moves_without(fwgc(Farmer, Wolf, Goat, Cabbage), [M|Ms], Visited) :-
     % Move: old state -> new state
-    safe_state(wfgc(Farmer, Wolf, Goat, Cabbage)),
-    safe_state(wfgc(OldF, OldW, OldG, OldC)),
-    move(wfgc(OldF, OldW, OldG, OldC), M, wfgc(Farmer, Wolf, Goat, Cabbage)),
-    nonmember_of(wfgc(OldF, OldW, OldG, OldC), Visited),
-    puzzlestate_move_without(wfgc(OldF, OldW, OldG, OldC), Ms, [wfgc(OldF, OldW, OldG, OldC)|Visited]).
+    safestate(fwgc(Farmer, Wolf, Goat, Cabbage)),
+    safestate(fwgc(OldF, OldW, OldG, OldC)),
+    move(fwgc(OldF, OldW, OldG, OldC), M, fwgc(Farmer, Wolf, Goat, Cabbage)),
+    nonmember_of(fwgc(OldF, OldW, OldG, OldC), Visited),
+    puzzlestate_moves_without(fwgc(OldF, OldW, OldG, OldC), Ms, [fwgc(OldF, OldW, OldG, OldC)|Visited]).
 
 
