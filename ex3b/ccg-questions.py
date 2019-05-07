5b842512d373eb077bdd06ea01808b60d338b1e7
Wenchang Liu <wenchang.liu@student.manchester.ac.uk>
2019-05-07 18:34:08 +0100

    slight changes on report

--- ex3b/ccg-questions.py
+++ ex3b/ccg-questions.py
@@ -2,35 +2,21 @@ from nltk.ccg import chart, lexicon
 
 lex = lexicon.fromstring('''
     :- S, N, NP
-    Steel => N {Steel}
-    is => (S\\N)/NP {\\x y.is(y, x)}
-    an => NP/N {\\P exist x.P(x)}
-    alloy => N {alloy}
-    contains => (S\\N)/N {\\x y.contains(y, x)}
-    carbon => N {carbon}
     Does => S/(S\\NP)/NP {\\P Q x.(P(x) & Q(x))}
     Ferrite => NP {Ferrite}
-    have => (S\\NP)/N {\\x y.have(y, x)}
+    have => (S\\NP)/N {\\x y.has(y, x)}
     high => N/N {\\x.high(x)}
     hardness => N {hardness}
     Which => S/(S\\NP)/N {\\P Q x.(P(x) & Q(x))}
     material => N {\\x.material(x)}
     has => (S\\NP)/NP {\\x y.has(y, x)}
-    the => NP/N {\\x. the(x)}
-    lowest => N/N/N {\\x y.argmin(x, y)}
+    the-lowest => NP/N/N {\\x y.argmin(x, y)}
     tensile => N  {tensile}
     strength => N {strength}
     ''', 
     True)
 
 parser = chart.CCGChartParser(lex, chart.DefaultRuleSet)
-for parse in parser.parse("Steel is an alloy".split()):
-    chart.printCCGDerivation(parse)
-    break
-
-for parse in parser.parse("Steel contains carbon".split()):
-    chart.printCCGDerivation(parse)
-    break
 
 for parse in parser.parse(
     "Does Ferrite have high hardness".split()):
@@ -38,6 +24,6 @@ for parse in parser.parse(
     break
 
 for parse in parser.parse(
-    "Which material has the lowest tensile strength".split()):
+    "Which material has the-lowest tensile strength".split()):
     chart.printCCGDerivation(parse)
     break
